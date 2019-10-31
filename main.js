b367b406608dad36c9ce1d8e50c713cb08d8cb71
kellycao <55064994+kellycao@users.noreply.github.com>
2019-10-31 18:30:53 -0400

    index_v1.1

--- main.js
+++ main.js
@@ -1,38 +1,13 @@
 document.documentElement.style.overflow='hidden';
+var headline = document.getElementById('headline');
+var a = document.getElementById('a');
+var b = document.getElementById('b');
 
-var headline = document.getElementsByTagName('h1');
-// function normal(){
-//   headline[0,1].style.transition = "all 3s";
-//   headline[0,1].style.paddingLeft = "2em";
-//   headline[0,1].style.opacity = "0.75";
-//   }
-// function transition(){
-// headline[0,1].style.transition = "all 3s";
-// headline[0,1].style.opacity = "1";
-//   headline[0].style.paddingLeft = "1.5em";
-//   headline[1].style.paddingLeft = "2.5em";
-//   }
-// headline[0,1].onmouseover = transition();
-// headline[0,1].onmouseout = normal();
-
-headline[0].onmouseover=function(){
-  this.style.transition = "all 3s";
-  this.style.opacity = "1";
-  this.style.paddingLeft = "1em";
+headline.onmouseover=function(){
+  a.style.transition = "all 3s";
+  b.style.transition = "all 3s";
+  a.style.transform="translateY(-50%)";
+  b.style.transform="translateY(50%)";
+  a.style.opacity= 1;
+  b.style.opacity= 1;
 }
- headline[0].onmouseout=function(){
-this.style.transition = "all 3s";
-   this.style.opacity = "0.75";
-   this.style.paddingLeft = "1.5em";
- }
-
- headline[1].onmouseover=function(){
-   this.style.transition = "all 3s";
-   this.style.opacity = "1";
-   this.style.paddingLeft = "2em";
- }
-  headline[1].onmouseout=function(){
- this.style.transition = "all 3s";
-    this.style.opacity = "0.75";
-    this.style.paddingLeft = "1.5em";
-  }
